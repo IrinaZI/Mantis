ee0b07718627e04f757eda62a75620f27abccfcd
IrinaZI <30446195+IrinaZI@users.noreply.github.com>
2017-11-05 19:17:26 +0300

    Добавлен тест test_login

---
+++ fixture/application.py
@@ -0,0 +1,36 @@
+
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(capabilities={"marionette": False})
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.session = SessionHelper(self)
+        self.base_url=base_url
+
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+
+    def open_login_page(self):
+        wd = self.wd
+        if not (wd.current_url.endswith("http://localhost/mantisbt-1.2.19/login_page.php") and wd.find_element_by_id("LoginForm")):
+            wd.get(self.base_url)
+
+
+    def destroy (self):
+        self.wd.quit()
